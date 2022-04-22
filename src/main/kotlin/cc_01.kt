0d4f46472d02dd30b0e9e5486f1e60603026de40
jobo8505 <Joern.Boegeholz@esolutions.de>
2022-04-22 09:07:38 +0200

    Initial commit

---
+++ src/main/kotlin/cc_01.kt
@@ -0,0 +1,10 @@
+/*
+    Coding Challenge #1
+    Write a program which prints all numbers between 1 and 365 to the console
+ */
+
+fun main(args: Array<String>) {
+    for(x in 1..10){
+        println("$x")
+    }
+}
