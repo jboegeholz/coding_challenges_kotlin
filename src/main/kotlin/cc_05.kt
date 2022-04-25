2ff9a3829ba60967e4d5af911ff8719f96783f80
jobo8505 <Joern.Boegeholz@esolutions.de>
2022-04-25 09:19:25 +0200

    add solution for cc_05

--- src/main/kotlin/cc_05.kt
+++ src/main/kotlin/cc_05.kt
@@ -1,3 +1,4 @@
+import kotlin.random.Random
 /*
     Coding Challenge #5
     Write a function which generates random passwords
@@ -11,5 +12,17 @@
  */
 
 fun main(args: Array<String>) {
+    val lowerAlphabet = ('a'..'z').toList()
+    val upperAlphabet = ('A'..'Z').toList()
+    val digits = (0..9).toList()
+    val specialChar = listOf('!', '”', '§', '$', '%', '&', '/', '?')
+    val allowedChars = lowerAlphabet + upperAlphabet + digits + specialChar
+
+    var password = ""
+    for(i in 0..8){
+        val myRand = Random.nextInt(0, allowedChars.size)
+        password += allowedChars[myRand]
+    }
+    println("The password is: $password")
 
 }
