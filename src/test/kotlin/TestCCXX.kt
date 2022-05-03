070d22f21e9c317627a47bb9cc2091da7b75df6f
jboegeholz <boegeholz.joern@gmail.com>
2022-05-03 10:05:22 +0200

    add test template

---
+++ src/test/kotlin/TestCCXX.kt
@@ -0,0 +1,14 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+/**
+ * Template for Unit Tests
+ */
+internal class TestCCXX {
+
+    @Test
+    fun testAssertTrue() {
+        assertTrue { true }
+    }
+}
